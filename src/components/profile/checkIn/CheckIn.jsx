0a7aa4a331842827592504297ca40598334f49f6
frede <frededv@outlook.dk>
2026-06-02 08:50:09 +0100

    commit

---
+++ src/components/profile/checkIn/CheckIn.jsx
@@ -0,0 +1,56 @@
+import {useState, useEffect} from "react";
+import ApiFacade from "../../../Api/ApiFacade.js";
+import "./CheckIn.css";
+export default function CheckIn({user, refresh}) {
+  const [activeCheckIns, setActiveCheckIns] = useState([]);
+
+ useEffect(() => {
+    // Simulate fetching check-in status from an API
+    const fetchCheckInStatus = async () => {
+      const result = await ApiFacade.getActiveCheckIns(user.id);
+      setActiveCheckIns(result);
+      console.log("user:", user);
+      console.log("result:", result);
+    }
+    fetchCheckInStatus();
+  }, [user.id, refresh]);
+
+
+  return (
+    <div className="check-in-container">
+  <h2>Aktive check-ins</h2>
+
+  <div className="active-checkins">
+    <p>Her kan du se alle aktive check-ins for dine børn.</p>
+
+    {activeCheckIns.length > 0 ? (
+      activeCheckIns.map((checkIn) => (
+        <div className="checkin-card" key={checkIn.id}>
+          <h3>Legeplads #{checkIn.playground_id}</h3>
+
+         <p>
+  <strong>
+    {checkIn.children.length > 1 ? "Børn" : "Barn"}:
+  </strong>{" "}
+  {checkIn.children.map(child => child.name).join(", ")}
+</p>
+          
+          <p>
+            <strong>Checket ind:</strong>{" "}
+            {new Date(
+              checkIn.check_in[0],
+              checkIn.check_in[1] - 1,
+              checkIn.check_in[2],
+              checkIn.check_in[3],
+              checkIn.check_in[4]
+            ).toLocaleString("da-DK")}
+          </p>
+        </div>
+      ))
+    ) : (
+      <p>Ingen aktive check-ins.</p>
+    )}
+  </div>
+</div>
+  );
+}
